3e280366b373b8a77abba2a0fcf7769c0b616d2d
Nebulaic Toaster <nebulaictoaster@gmail.com>
2016-09-28 17:12:21 +0100

    Nearly got nextto working
    got to work out the fact that the list isnt circular

--- TablePlan3.py
+++ TablePlan3.py
@@ -32,8 +32,75 @@ class Table:
 	def TableGenerate(self):
 		boys=self.boys
 		girls=self.girls
+		table = []
 		shuffle(boys)
 		shuffle(girls)
+		n=0
+		while len(boys) != 0:
+			boy = boys[n]
+			nextboy = boys[n+1] 
+			nexttolist = self.NextTo(boy)
+			print(nexttolist)
+			if len(nexttolist) == 1:
+				if nexttolist[0] in table:
+					index = table.index(nexttolist[0])
+					if table[index-1] in boys:
+						table.insert(index+1, boy)
+					else:
+						table.insert(index, boy)
+					boys.pop(boys.index(boy))
+				else:
+					table.append(None)
+					table.append(boy)
+					boys.pop(boys.index(boy))
+					table.append(nexttolist[0])
+					girls.pop(girls.index(nexttolist[0]))
+			elif len(nexttolist)==2:
+				if (nexttolist[0] in table) and (nexttolist[1] in table):
+					i = table.index(nexttolist[0])
+					i2 = table.index(nexttolist[1])
+					if i<i2:
+						table.insert(i2, boy)
+					else: 
+						table.insert(i, boy)
+					boys.pop(boys.index(boy))
+				elif nexttolist[0] in table:
+					index = table.index(nexttolist[0])
+					if table[index-1] in boys:
+						table.insert(index, nexttolist[1])
+						table.insert(index, boy)
+					
+					else:
+						table.insert(index, boy)
+						table.insert(index, nexttolist[1])
+					boys.pop(boys.index(boy))
+					girls.pop(girls.index(nexttolist[1]))
+				elif nexttolist[1] in table:
+					index = table.index(nexttolist[1])
+					if table[index-1] in boys:
+						table.insert(index, nexttolist[0])
+						table.insert(index, boy)
+					
+					else:
+						table.insert(index, boy)
+						table.insert(index, nexttolist[0])
+					boys.pop(boys.index(boy))
+					girls.pop(girls.index(nexttolist[0]))
+				else:
+					table.append(nexttolist[0])
+					table.append(boy)
+					table.append(nexttolist[1])
+					boys.pop(boys.index(boy))
+					
+					girls.pop(girls.index(nexttolist[0]))
+					girls.pop(girls.index(nexttolist[1]))
+			else:
+				table.append(None)
+				table.append(boy)
+				table.append(None)
+				boys.pop(boys.index(boy))
+			n = boys.index(nextboy)
+		print(table)		
 		
 		
 	
@@ -64,16 +131,21 @@ class Table:
 		else:
 			return False
 	def NextTo(self, name):
+		
 		if name in self.boys:
+			nexttolist = []
 			for n in range(0, len(self.rules)-1):
+				rule = self.rules[n]
 				if rule[0] ==name and rule[2] == 'next':
-					return rule[1]
+					nexttolist.append(rule[1])
 		elif name in self.girls:
+			nexttolist = []
 			for n in range(0, len(self.rules)-1):
+				rule = self.rules[n]
 				if rule[0] ==name and rule[2] == 'next':
-					return rule[0]
-		return False
-	def NotNextTo(self, boy, girl):
+					nexttolist.append(rule[0])
+		return nexttolist
+	def NotNextTo(self, name):
 		if name in self.boys:
 			for n in range(0, len(self.rules)-1):
 				if rule[0] ==name and rule[2] == 'notnext':
@@ -107,14 +179,14 @@ class Table:
 				print("sorry, those names or rules weren't applicable")
 				
 	def TestTable(self):
-		self.TablePlan()
+		#self.TablePlan()
 		self.TableGenerate()
-		x="\n"
-		while x != "q":
-			x=input("")
-			self.CheckRules()
+		#x="\n"
+		#while x != "q":
+		#	x=input("")
+		#	self.CheckRules()
 			
-			self.DrawTable()
+		#	self.DrawTable()
 		
 	def DrawTable(self):
 		table=self.table
@@ -122,7 +194,7 @@ class Table:
 		
 		for i in range(len(table)):
 			print(table[i])
-	def NextTo(self, person):
+	def Old_NextTo(self, person):
 		table = self.table
 		i = table.index(person)
 		current_pos = i
@@ -140,7 +212,7 @@ class Table:
 
 
 boys = ["Jim Bruges", "Will 'It shows' Gough", "James 'NUT' Gardiner", "Yergus 'Stupid Yerger' O'Keefe", "Tom Jewson", "Tom Middleton", "Horatio 'meme man' Lovering", "Bigmac Macmullen", "Arron 'Weeb' Oliphant"]
-girls= ["Charlotte Pender", "Charlotte Ashley", "Princess Fiona Bennett", "Emily Stainer", "Sophie 'Nietszche' Ashley", "Izzy Mckellar", "Imi Colla", "Alice Walton-Knight", "George 'meme' Watton"]
+girls= ["Charlotte Ashley", "Charlotte Pender", "Princess Fiona Bennett", "Emily Stainer", "Sophie 'Nietszche' Ashley", "Izzy Mckellar", "Imi Colla", "Alice Walton-Knight", "George 'meme' Watton"]
 rules= [["Bigmac Macmullen","Imi Colla", "next"], ["Arron 'Weeb' Oliphant", "Princess Fiona Bennett", "next"], ["Arron 'Weeb' Oliphant", "Emily Stainer", "next"], ["Bigmac Macmullen", "Emily Stainer", "next"], ["Tom Jewson", "Charlotte Pender", "next"], ["Jim Bruges", "George 'meme' Watton", "next"], ["Yergus 'Stupid Yerger' O'Keefe", "Charlotte Pender", "next"], ["James 'NUT' Gardiner", "Princess Fiona Bennett", "next"],["James 'NUT' Gardiner", "Princess Fiona Bennett", "notnext"] ]
 
 
@@ -153,7 +225,7 @@ print(table.NextTo("Watton"))
 
 
 
-for i in range(0, len(table.table)):
+'''for i in range(0, len(table.table)):
 	
 	nextto = table.NextTo(table.table[i])
 	print("Seating Plan organised by a computer\n")
@@ -162,6 +234,6 @@ for i in range(0, len(table.table)):
 	print(random.choice(["Be nice and make conversation.", "Don't be a meme", "It shows.", "Make a speech", "No Brexit chat", "Blame the computer for who you're sitting next to", "If Jim failed his driving test expect nothing from tonight."]))
 	
 
-
+'''
 
 input('Press ENTER to continue')
